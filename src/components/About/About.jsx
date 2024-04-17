bb73827c4e11d8b7c51af67759c030ad44df14e7
Pratham Dubey <134331217+prathamdby@users.noreply.github.com>
2024-04-17 20:52:19 +0530

    feat!: Implement the about component
    Signed-off-by: Pratham Dubey <134331217+prathamdby@users.noreply.github.com>

---
+++ src/components/About/About.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styles from "./About.module.css";
+import { getImageURL } from "../../utilities";
+
+const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          className={styles.aboutImage}
+          src={getImageURL("about/aboutImage.png")}
+          alt="about-image"
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageURL("about/cursorIcon.png")} alt="cursor-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageURL("about/serverIcon.png")} alt="server-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimized back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageURL("about/uiIcon.png")} alt="ui-icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI/UX Designer</h3>
+              <p>
+                I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default About;
